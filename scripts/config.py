d4ea1e3d57dedfdeb00c37e34d2daa9c21de3f9e
Aman Saraf <amansaraf99@gmail.com>
2021-11-21 19:21:07 -0800

    Added UDP Control Streaming to iOS
    Can send steering and throttle to iOS device specified in config file

---
+++ scripts/config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+config = {
+    "ip_address": "10.0.0.34",
+    "G": 9.8,
+    "query_rate": 50
+}
---
+++ scripts/control_streamer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+from carla_msgs.msg import CarlaEgoVehicleControl
+import socket
+import config as cfg
+
+
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+host = cfg.config["ip_address"] ### Local address of iPhone
+port = 8004 ### Port for Control Signals on iPhone
+
+def send_control_over_udp(data):
+    msg = str(data.throttle) + "," + str(data.steer)
+    udp_socket.sendto(msg.encode('utf-8'), (host,port))
+
+def ControlStreamer():
+    rospy.init_node('control_streamer', anonymous=False)
+    rospy.Subscriber('/carla/ego_vehicle/vehicle_control_cmd_manual', CarlaEgoVehicleControl, send_control_over_udp)
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        ControlStreamer()
+    except rospy.ROSInterruptException:
+        pass
