bc4b407eed884c55bdd06aaeeac30923b09fe653
amansrf <amansaraf99@gmail.com>
2022-03-15 15:52:30 -0700

    Added Buffer_Length as config variable and Removed Redundant Code

--- ros_roar_streamer/config.py
+++ ros_roar_streamer/config.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 config = {
-    "ip_address": "10.0.0.4",
+    "ip_address": "10.0.0.12",
     "G": 9.8,
-    "query_rate": 0.02
+    "query_rate": 0.02,
+    "buffer_length": 1,
 }
