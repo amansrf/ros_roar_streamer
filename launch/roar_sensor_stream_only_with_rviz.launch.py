c70ab67b7da76daf58b3cce3f05e998eff464b16
Yi-Shuan <yihsuan_sung@berkeley.edu>
2022-05-05 13:50:05 -0700

    nav

--- launch/roar_sensor_stream_only_with_rviz.launch.py
+++ launch/roar_sensor_stream_only_with_rviz.launch.py
@@ -5,6 +5,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 import launch
 import launch_ros
+from nav2_common.launch import RewrittenYaml
 
 
 def generate_launch_description():
@@ -13,9 +14,49 @@ def generate_launch_description():
         get_package_share_directory("ros_roar_streamer")
     )  # also tried without realpath
     rviz_path = base_path + "/config/default_config.rviz"
+    namespace = launch.substitutions.LaunchConfiguration('namespace')
+    map_yaml_file = launch.substitutions.LaunchConfiguration('map')
+    use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time')
+    autostart = launch.substitutions.LaunchConfiguration('autostart')
+    params_file = launch.substitutions.LaunchConfiguration('params_file')
+    lifecycle_nodes = ['map_server']
+    params_file = launch.substitutions.LaunchConfiguration('params_file')
 
+    param_substitutions = {
+        'use_sim_time': use_sim_time,
+        'yaml_filename': map_yaml_file
+        }
+    
+    configured_params = RewrittenYaml(
+        source_file=params_file,
+        root_key=namespace,
+        param_rewrites=param_substitutions,
+        convert_types=True)
+    
     return LaunchDescription(
         [
+            launch.actions.DeclareLaunchArgument(
+                'namespace', default_value='',
+                description='Top-level namespace'),
+
+            launch.actions.DeclareLaunchArgument(
+                'map',
+                default_value=os.path.join(base_path, 'maps', 'turtlebot3_world.yaml'),
+                description='Full path to map yaml file to load'),
+
+            launch.actions.DeclareLaunchArgument(
+                'use_sim_time', default_value='false',
+                description='Use simulation (Gazebo) clock if true'),
+
+            launch.actions.DeclareLaunchArgument(
+                'autostart', default_value='true',
+                description='Automatically startup the nav2 stack'),
+            
+            launch.actions.DeclareLaunchArgument(
+                'params_file',
+                default_value='/home/roar/roar/roar_ros/src/ros_roar_streamer/params/nav2_params.yaml',
+                description='Full path to the ROS2 parameters file to use'
+            ),
             launch.actions.DeclareLaunchArgument(
                 name="ios_ip_address", default_value="127.0.0.1",
             ),
@@ -93,6 +134,48 @@ def generate_launch_description():
                         )
                     },
                 ],
+            ),
+            Node(
+                package='nav2_planner',
+                executable='planner_server',
+                name='planner_server',
+                output='screen',
+                parameters=[configured_params],
+            ),
+            Node(
+                package='nav2_bt_navigator',
+                executable='bt_navigator',
+                name='bt_navigator',
+                output='screen',
+                parameters=[configured_params],
+            ),
+            # Node(
+            #     package="nav2_costmap_2d",
+            #     name="costmap_node",   
+            #     parameters=[
+            #         os.path.join(base_path, 'params', 'nav2_params.yaml')
+            #     ], 
+            # ),
+            Node(
+                package='nav2_map_server',
+                executable='map_server',
+                name='map_server',
+                parameters=[configured_params],
+            ),
+            Node(
+                package='nav2_controller',
+                executable='controller_server',
+                output='screen',
+                parameters=[configured_params],
+            ),
+            Node(
+                package='nav2_lifecycle_manager',
+                executable='lifecycle_manager',
+                name='lifecycle_manager_navigation',
+                output='screen',
+                parameters=[{'use_sim_time': use_sim_time},
+                            {'autostart': autostart},
+                            {'node_names': lifecycle_nodes}]
             ),
             # Node(
             #     package="ros_roar_streamer",
